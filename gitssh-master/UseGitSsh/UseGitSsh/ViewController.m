7eab945e328e69eb00e3eae83bbf78983cbf2513
zhangjie02 <zhangjie02@inke.cn>
2019-03-04 15:32:57 +0800

    提交已有项目

---
+++ gitssh-master/UseGitSsh/UseGitSsh/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  UseGitSsh
+//
+//  Created by zhangjie on 2019/3/4.
+//  Copyright © 2019年 Elohim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //初始化项目
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor purpleColor];
+    view.frame = CGRectMake(100, 200, 200, 100);
+    [self.view addSubview:view];
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+
+@end
